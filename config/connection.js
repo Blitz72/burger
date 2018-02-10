f81ebb98bcbb47f9872b7cde632a5f38bcda6c47
David Bauer <bauer.david@att.net>
2018-02-09 21:07:10 -0500

    modified conection.js

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 // Set up MySQL connection.
 require("dotenv").config();
 var mysql = require("mysql");
-var keys = require("../keys.js");
+// var keys = require("../keys.js");
 
 // var connection = mysql.createConnection(
 //   keys.mysqlKeys
@@ -10,9 +10,13 @@ var keys = require("../keys.js");
 if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-	var connection = mysql.createConnection(
-	  keys.mysqlKeys
-	);
+	var connection = mysql.createConnection({
+		host: 'localhost',
+		port: '3306',
+		user: 'root',
+		password: 'love2MySQL2@CWRU',
+		database: 'burgers_db'
+	});
 }
 
 // Make connection.
